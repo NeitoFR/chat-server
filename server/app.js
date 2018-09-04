50ea04db895df88a4525b3f8fd232d392d6b602c
Willem Houm <willem.houm@gmail.com>
2018-09-04 12:29:50 +0200

    trying timeout

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 var http = require('http'),
-    server = http.createServer().listen(process.env.APP_PORT, function(){console.log('Server listening on port : '+process.env.APP_PORT)}),
+    server = http.createServer().listen(process.env.APP_PORT, function () { console.log('Server listening on port : ' + process.env.APP_PORT) }),
     io = require('socket.io').listen(server);
 
 var users = [];
@@ -12,10 +12,14 @@ io.sockets.on('connection', function (socket) {
     console.log('Un client est connecté !');
     socket.on('username', function (message) {
         socket.username = message.username;
-        users.push({username: socket.username});
+        users.push({ username: socket.username });
         _sendUserList();
     })
 });
+
+io.sockets.on('connect_timeout', function (message) {
+    console.log('**** Timed OUT ', message);
+})
 
 // server.listen(process.env.APP_PORT, function () {
 //     console.log('Server listening on port : ' + process.env.APP_PORT);
