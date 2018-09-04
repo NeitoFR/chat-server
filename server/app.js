d657b1853f55a87a8a322acc00a178c6fb940619
Willem Houm <willem.houm@gmail.com>
2018-09-04 12:03:00 +0200

    Init project

---
+++ server/app.js
@@ -0,0 +1,26 @@
+"use strict"
+//require
+require('dotenv').config();
+
+var http = require('http'),
+    server = http.createServer().listen(process.env.APP_PORT, function(){console.log('Server listening on port : '+process.env.APP_PORT)}),
+    io = require('socket.io').listen(server);
+
+var users = [];
+
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+    socket.on('username', function (message) {
+        socket.username = message.username;
+        users.push({username: socket.username});
+        _sendUserList();
+    })
+});
+
+// server.listen(process.env.APP_PORT, function () {
+//     console.log('Server listening on port : ' + process.env.APP_PORT);
+// });
+
+function _sendUserList() {
+    io.emit('userlist', users);
+}
